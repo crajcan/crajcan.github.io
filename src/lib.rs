840f8ccc809efe84eadc4c30650597a06fddc6d6
Carson Rajcan <carajcan@gmail.com>
2022-08-24 23:07:06 -0500

    Get shallow but declarative app running

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,64 @@
-use wasm_bindgen::prelude::*;
+use derive_more::{Display, Error};
+use std::error::Error;
 
+use wasm_bindgen::prelude::*;
+use web_sys::{Document, Element, Window};
+
+// Called when the wasm module is instantiated
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let dom = window.document().expect("should have a document on window");
+    let shell = dom
+        .get_element_by_id("slider")
+        .expect("should have an app!");
+
+    let app = App::new();
+
+    shell.append_child(&app.render(dom))?;
+    log("I can log from Rust too!");
+
+    Ok(())
+}
+
+struct App {
+    slider: Vec<Vec<i32>>,
+}
+
+impl App {
+    fn new() -> App {
+        App {
+            slider: vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]],
+        }
+    }
+
+    fn render(&self, dom: Document) -> Element {
+        let app = dom.create_element("div").unwrap();
+        app.set_inner_html("Hello from declarative App!");
+
+        app
+    }
+}
+
+#[derive(Debug, Display, Error)]
+struct myError {
+    message: String,
+}
+
+impl From<JsValue> for myError {
+    fn from(error: JsValue) -> Self {
+        myError {
+            message: format!("{:?}", error),
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn add(a: u32, b: u32) -> u32 {
+    a + b
+}
 
 #[wasm_bindgen]
 extern "C" {
@@ -17,30 +76,4 @@ extern "C" {
     // Multiple arguments too!
     #[wasm_bindgen(js_namespace = console, js_name = log)]
     fn log_many(a: &str, b: &str);
-}
-
-
-// Called when the wasm module is instantiated
-#[wasm_bindgen(start)]
-pub fn main() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
-    let window = web_sys::window().expect("no global `window` exists");
-    let dom = window.document().expect("should have a document on window");
-    let app = dom
-        .get_element_by_id("slider")
-        .expect("should have an app!");
-
-    let val = dom.create_element("div")?;
-    val.set_inner_html("Hello from div app!");
-
-    app.append_child(&val)?;
-    log("I can log from Rust too!");
-
-    Ok(())
-}
-
-#[wasm_bindgen]
-pub fn add(a: u32, b: u32) -> u32 {
-    a + b
 }

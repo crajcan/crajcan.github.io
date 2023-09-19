3165ac5ebd55d103004c6549f3199c077c5c3163
Carson Rajcan <carajcan@gmail.com>
2023-09-19 11:46:28 -0500

    Try bumping hash

--- rust_jobs/dist/rust_jobs-b2c80ded105dce3b.js
+++ rust_jobs/dist/rust_jobs-b2c80ded105dce3b.js
@@ -633,7 +633,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('rust_jobs-3c0ca847978df698_bg.wasm', import.meta.url);
+        input = new URL('rust_jobs-b2c80ded105dce3b_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
--- rust_jobs/src/app.rs
+++ rust_jobs/src/app.rs
@@ -6,7 +6,7 @@ pub fn app() -> Html {
         <main>
             <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
             <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+            <span class="subtitle">{ "from Carson with " }<i class="heart" /></span>
         </main>
     }
 }

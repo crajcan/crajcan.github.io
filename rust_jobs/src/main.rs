c54cf88c981d32b19ca18a737f25b9516ce8eb6e
Carson Rajcan <carajcan@gmail.com>
2023-09-20 16:08:43 -0500

    Get interactivity working in Yew tutorial

--- rust_jobs/src/main.rs
+++ rust_jobs/src/main.rs
@@ -3,5 +3,14 @@ mod app;
 use app::App;
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    let document = gloo::utils::document();
+    let body = document.query_selector("body").unwrap().unwrap();
+
+    let yew_mount = document.create_element("div").unwrap();
+    let classes = yew_mount.class_list();
+    classes.add_1("yew-mount").unwrap();
+
+    body.append_child(&yew_mount).unwrap();
+
+    yew::Renderer::<App>::with_root(yew_mount).render();
 }

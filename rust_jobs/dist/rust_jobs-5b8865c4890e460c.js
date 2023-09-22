29b6c0ba91852c0d89b496178c1989a8254fd3cd
Carson Rajcan <carajcan@gmail.com>
2023-09-22 16:53:29 -0500

    Update company meta styling

--- rust_jobs/dist/rust_jobs-5b8865c4890e460c.js
+++ rust_jobs/dist/rust_jobs-5b8865c4890e460c.js
@@ -702,12 +702,12 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper500 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 203, __wbg_adapter_18);
+    imports.wbg.__wbindgen_closure_wrapper512 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 205, __wbg_adapter_18);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper701 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 288, __wbg_adapter_21);
+    imports.wbg.__wbindgen_closure_wrapper718 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 290, __wbg_adapter_21);
         return addHeapObject(ret);
     };
 
@@ -749,7 +749,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('rust_jobs-e679a18f08c911_bg.wasm', import.meta.url);
+        input = new URL('rust_jobs-5b8865c4890e460c_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 

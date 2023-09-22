1dc6efe1aae4f7d7d666aa7e679bcd596a26d554
Carson Rajcan <carajcan@gmail.com>
2023-09-22 17:37:16 -0500

    Build updates to .company-meta and .main

--- rust_jobs/dist/rust_jobs-f6a5f632dbad60f6.js
+++ rust_jobs/dist/rust_jobs-f6a5f632dbad60f6.js
@@ -749,7 +749,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('rust_jobs-5b8865c4890e460c_bg.wasm', import.meta.url);
+        input = new URL('rust_jobs-f6a5f632dbad60f6_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
